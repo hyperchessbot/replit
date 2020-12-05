8ddacfb7a49a71eec6b9e494945732963a1519e6
Chessbot Hyper <hyperchessbot@gmail.com>
2020-12-05 06:48:49 +0000

    Day5 full

--- adventofcode/20201205/main.scala
+++ adventofcode/20201205/main.scala
@@ -1,14 +1,22 @@
-import scala.util.chaining._
-
 object Main extends App {
   val inputs = scala.io.Source.fromFile("input.txt").getLines.toList
 
-  inputs
+  val occupSeatIds = inputs
     .map(_
       .replaceAll("B|R", "1")
       .replaceAll("F|L", "0")
     )
     .map(Integer.parseInt(_, 2))
-    .max
-    .tap(println)
+    
+  println(s"max seat ID ${occupSeatIds.max}")
+
+  for(i <- 1 until 1023) {
+    if(
+      !occupSeatIds.contains(i)
+      && occupSeatIds.contains(i - 1)
+      && occupSeatIds.contains(i + 1)
+    ) {
+      println(s"missing seat ID satisfying constraints $i")
+    }
+  }
 }
