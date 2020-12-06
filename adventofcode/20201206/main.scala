06e0cf78dc132b28d1428cb1ada4cd62023e84ee
Chessbot Hyper <hyperchessbot@gmail.com>
2020-12-06 13:23:11 +0000

    Use shorthands

--- adventofcode/20201206/main.scala
+++ adventofcode/20201206/main.scala
@@ -4,13 +4,13 @@ object Main extends App {
 
     val groups = scala.io.Source
       .fromFile(s"$input.txt").mkString("").split("\n\n")
-      .map(_.split("\n").map(_.split("").toSet).toList)
-      .toList
+      .map(_.split("\n").map(_.split("").toSet))
 
-    def countYesAnsweredQuestions(all:Boolean)(group:List[Set[String]]):Int = group
-      .reduce((a, b) => if(all) a.intersect(b) else a.union(b))
-      .count(_ => true)
+    def countYesAnsweredQuestions(all:Boolean)(group:Array[Set[String]]):Int = (if(all)
+      group.reduce(_.intersect(_)) else
+      group.reduce(_.union(_)))
+      .size
 
-    for(all <- List(false, true)) println(s"yes by ${if(all) "all " else "some"} = ${groups.map(countYesAnsweredQuestions(all)).sum}")
+    for(all <- List(false, true)) println(s"yes by ${if(all) "all " else "some"} = ${groups.map( countYesAnsweredQuestions(all)).sum}")
   }
 }
