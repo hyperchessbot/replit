5c14f3fd7ef412b01ff471d4081dad4f48f3deae
Chessbot Hyper <hyperchessbot@gmail.com>
2020-12-06 10:42:26 +0000

    Code structure Day 6

--- adventofcode/20201206/main.scala
+++ adventofcode/20201206/main.scala
@@ -2,14 +2,14 @@ object Main extends App {
   for(input <- List("example", "input")){    
     println(s"\nfor '$input' :\n")
 
-    val groups = scala.io.Source.fromFile(s"$input.txt").mkString("").split("\n\n").toList
+    val groups = scala.io.Source
+      .fromFile(s"$input.txt").mkString("").split("\n\n")
+      .map(_.split("\n").map(_.split("").toSet).toList)
+      .toList
 
-    def countYesAnsweredQuestions(all:Boolean)(group:String):Int = {
-      group.split("\n")
-        .map(_.split("").toSet)
-        .reduce((a, b) => if(all) a.intersect(b) else a.union(b))
-        .count(_ => true)      
-    }
+    def countYesAnsweredQuestions(all:Boolean)(group:List[Set[String]]):Int = group
+      .reduce((a, b) => if(all) a.intersect(b) else a.union(b))
+      .count(_ => true)
 
     for(all <- List(false, true)) println(s"yes by ${if(all) "all " else "some"} = ${groups.map(countYesAnsweredQuestions(all)).sum}")
   }
