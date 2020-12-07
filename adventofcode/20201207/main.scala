13d95cf98aa5ab126afb8eebd4b77858fcdf20bb
Chessbot Hyper <hyperchessbot@gmail.com>
2020-12-07 08:20:05 +0000

    Day 7 Scala first question

--- adventofcode/20201207/main.scala
+++ adventofcode/20201207/main.scala
@@ -2,9 +2,23 @@ object Main extends App {
   for(input <- List("example", "input")){    
     println(s"\nfor '$input' :\n")
 
-    val inputStr = scala.io.Source
-      .fromFile(s"$input.txt").mkString("")
+    def toColor(color:String) = color.replaceAll("^\\d+ | bag$| bag\\.$| bags$| bags\\.$", "")
 
-    println(s"input size = ${inputStr.length}")
+    case class Rule(ruleStr: String){
+      val parts = ruleStr.split(" contain |, ")
+      val color = toColor(parts(0))
+      val contains = parts.tail.map(toColor).toSet      
+      def canContain(color:String, rules:List[Rule]):Boolean = {
+        if(contains.contains(color)) return true        
+        for(rule <- rules.filter(rule => contains.contains(rule.color))) if(rule.canContain(color, rules)) return true
+        false
+      }
+    }
+
+    val rules = scala.io.Source.fromFile(s"$input.txt").getLines.map(Rule(_)).toList
+
+    val requiredColor = "shiny gold"
+
+    println(s"number of bag colors that can contain $requiredColor = ${rules.filter(_.canContain(requiredColor, rules)).size}")
   }
 }
--- adventofcode/20201207/run.sh
+++ adventofcode/20201207/run.sh
@@ -1,6 +1,6 @@
 echo "cleaning"
 rm *.class
 echo "compiling"
-scalac -classpath . -d . main.scala
+scalac -deprecation -classpath . -d . main.scala
 echo "running"
 scala -classpath . Main
